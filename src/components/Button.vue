329da53cfc44605d11cccf62f603d6eeac7e4197
Sinan <sinanaydogan@hotmail.com.tr>
2021-02-24 15:21:46 +0300

    Fixing

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button type="button" :class="'w-full text-center bg-'+color+'-400 border-1 shadow border-'+color+'-700 text-gray-100 px-2 py-4 text-center text-2xl font-semibold hover:bg-'+color+'-600'">{{ label }}</button>
+    <button type="button" :class="'w-full text-center bg-'+color+'-500 border-1 shadow border-'+color+'-700 text-gray-100 px-2 py-4 text-center text-2xl font-semibold hover:bg-'+color+'-600'">{{ label }}</button>
 </template>
 <script>
 export default {
